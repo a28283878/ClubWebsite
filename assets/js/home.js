ab3e513d1327dc5e50061d7fea000694f032f8f9
zazaman <a28283878@gmail.com>
2016-09-13 21:01:16 +0800

    aaa

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,8 +6,12 @@ window.addEventListener("scroll", function() {
     }
 }, false);
 
+window.addEventListener("scroll", function() {
+    if(window.scrollY > 350) $('.section1').fadeTo(400, 1);
+}, false);
+
 $('.lesson').click(function(event) {
-	var lessonId = "#" + $(this).attr('value');
+    var lessonId = "#" + $(this).attr('value');
     $(lessonId).fadeIn();
     $('#dark').fadeIn();
 })
